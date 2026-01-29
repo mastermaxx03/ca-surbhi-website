bd7929c0004b6be45bf945c7212996e2e9362f73
mastermaxx03 <srivastavaanimesh22@gmail.com>
2026-01-29 22:28:34 +0530

    fix: follower count correction

--- src/content/social.ts
+++ src/content/social.ts
@@ -1,51 +1,58 @@
 export interface SocialPlatform {
-  id: string
-  name: string
-  icon: 'youtube' | 'instagram' | 'telegram' | 'facebook' | 'whatsapp' | 'twitter' | 'threads'
-  followers: string
-  link: string | null
+  id: string;
+  name: string;
+  icon:
+    | "youtube"
+    | "instagram"
+    | "telegram"
+    | "facebook"
+    | "whatsapp"
+    | "twitter"
+    | "threads";
+  followers: string;
+  link: string | null;
 }
 
 export const socialPlatforms: SocialPlatform[] = [
   {
-    id: 'youtube',
-    name: 'YouTube',
-    icon: 'youtube',
-    followers: '1M+',
-    link: 'https://youtube.com/@casurbhisrivastavabhartiya',
+    id: "instagram",
+    name: "Instagram",
+    icon: "instagram",
+    followers: "800K+",
+    link: "https://www.instagram.com/surbhisrivastava08",
   },
   {
-    id: 'instagram',
-    name: 'Instagram',
-    icon: 'instagram',
-    followers: '500K+',
-    link: 'https://www.instagram.com/surbhisrivastava08',
+    id: "facebook",
+    name: "Facebook",
+    icon: "facebook",
+    followers: "600K+",
+    link: "https://www.facebook.com/share/1GjX4nWzni/?mibextid=wwXIfr",
   },
   {
-    id: 'facebook',
-    name: 'Facebook',
-    icon: 'facebook',
-    followers: '300K+',
-    link: 'https://www.facebook.com/share/1GjX4nWzni/?mibextid=wwXIfr',
+    id: "threads",
+    name: "Threads",
+    icon: "threads",
+    followers: "100K+",
+    link: "https://www.threads.com/@surbhisrivastava08?igshid=NTc4MTIwNjQ2YQ==",
   },
   {
-    id: 'twitter',
-    name: 'X (Twitter)',
-    icon: 'twitter',
-    followers: '100K+',
-    link: 'https://x.com/surbhi_bhartiya?s=21',
+    id: "twitter",
+    name: "X (Twitter)",
+    icon: "twitter",
+    followers: "1K+",
+    link: "https://x.com/surbhi_bhartiya?s=21",
   },
   {
-    id: 'threads',
-    name: 'Threads',
-    icon: 'threads',
-    followers: '50K+',
-    link: 'https://www.threads.com/@surbhisrivastava08?igshid=NTc4MTIwNjQ2YQ==',
+    id: "youtube",
+    name: "YouTube",
+    icon: "youtube",
+    followers: "1K+",
+    link: "https://youtube.com/@casurbhisrivastavabhartiya",
   },
-]
+];
 
 export const socialSection = {
-  title: 'Connect With Us on Social Media',
-  buttonText: 'Follow',
-  comingSoonText: 'Coming soon',
-}
+  title: "Connect With Us on Social Media",
+  buttonText: "Follow",
+  comingSoonText: "Coming soon",
+};
